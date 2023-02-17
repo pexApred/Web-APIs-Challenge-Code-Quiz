4dfb1827ea20f77f9d3a9be529eb9ca886131787
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-17 09:20:01 -0500

    added filler js to check functionality

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+// Start Button Function correctly filler
+var startButton = document.querySelector("#start-button");
+
+startButton.addEventListener("click", function() {
+    document.location.href="https://www.pizzatimesc.com/";
+});
