62ade3c7f2331ad13f289dfbcb10f4d90f6bceb0
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-18 14:20:42 -0500

    added HTML & JS content for start quiz button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,50 @@
 // Start Button Function correctly filler
 var startButton = document.querySelector("#start-button");
+var quizContainer = document.getElementById("quiz");
+const Questions = [ 
+{
+    question: "Commonly used data types DO NOT include:",
+    choices:  ["strings, booleans, alerts, numbers"],
+    correctAnswer: "alerts"
+},
+{
+    question: "Commonly used data types DO NOT include:",
+    choices:  ["strings, booleans, alerts, numbers"],
+    correctAnswer: "alerts"
+},
+{
+    question: "Commonly used data types DO NOT include:",
+    choices:  ["strings, booleans, alerts, numbers"],
+    correctAnswer: "alerts"
+},
+{
+    question: "Commonly used data types DO NOT include:",
+    choices:  ["strings, booleans, alerts, numbers"],
+    correctAnswer: "alerts"
+},
+{
+    question: "Commonly used data types DO NOT include:",
+    choices:  ["strings, booleans, alerts, numbers"],
+    correctAnswer: "alerts"
+},
+];
 
 startButton.addEventListener("click", function() {
-    document.location.href="https://www.pizzatimesc.com/";
+    var newPage = document.querySelector(".start-pg");
+    if (newPage.style.display === "none") {
+        newPage.style.display = "block";
+    
+    } else {
+        newPage.style.display = "none";
+    };
+
+    // var questions = document.querySelector(".questions");
+    // if (questions.style.display === "block") {
+    //     questions.style.display = "none";
+    
+    // } else {
+    //     questions.style.display = "block";
+    // }
+
 });
+
