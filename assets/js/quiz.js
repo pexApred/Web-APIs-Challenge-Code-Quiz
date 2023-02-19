97d5431e3fab496b40d6ee9791625051d46fc677
pexApred <120075705+pexApred@users.noreply.github.com>
2023-02-19 14:06:01 -0500

    fixed timer to start when start quiz button pressed

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -9,31 +9,31 @@ var currentQuestionIndex = 0;
 let currentTime = timeLeft.textContent;
 // Array of objects: Quiz Questions
 const questions = [ 
-{
-    question: "Commonly used data types DO NOT include:",
-    choices:  ["strings, booleans, alerts, numbers"],
-    correctAnswer: "alerts"
-},
-{
-    question: "The condition in an if / else statement is enclosed with ________.",
-    choices:  ["quotes, curly brackets, parenthesis, square brackets"],
-    correctAnswer: "parenthesis"
-},
-{
-    question: "Arrays in JavaScript can be used to store ________.",
-    choices:  ["numbers and strings, other arrays, booleans, all of the above"],
-    correctAnswer: "all of the above"
-},
-{
-    question: "String values must be enclosed within ________ when being assigned to variables",
-    choices:  ["commas, curly brackets, quotes,parenthesis"],
-    correctAnswer: "quotes"
-},
-{
-    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices:  ["JavaScript, terminal/bash, for loops, console.log"],
-    correctAnswer: "console.log"
-},
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices:  ["strings, booleans, alerts, numbers"],
+        correctAnswer: "alerts"
+    },
+    {
+        question: "The condition in an if / else statement is enclosed with ________.",
+        choices:  ["quotes, curly brackets, parenthesis, square brackets"],
+        correctAnswer: "parenthesis"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store ________.",
+        choices:  ["numbers and strings, other arrays, booleans, all of the above"],
+        correctAnswer: "all of the above"
+    },
+    {
+        question: "String values must be enclosed within ________ when being assigned to variables",
+        choices:  ["commas, curly brackets, quotes,parenthesis"],
+        correctAnswer: "quotes"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        choices:  ["JavaScript, terminal/bash, for loops, console.log"],
+        correctAnswer: "console.log"
+    },
 ];
 
 // Push button start: Removes start-pg, starts timer function, for loop questions, results of answers (correct or time penalty)
@@ -43,6 +43,18 @@ startButton.addEventListener("click", function() {
     quizContainer.style.display ="block";
     showCurrentQuestion();
     
+    let intervalId = setInterval(startTimer, 1000);
+
+    // Timer function
+    function startTimer () {
+        currentTime--;
+        timeLeft.textContent = currentTime;
+
+        if (currentTime === 0) {
+            clearInterval(intervalId);
+            alert("GAME OVER! Your final score is + result");
+        }
+    }
 });
 
 // Write showCurrentQuestion() function
@@ -58,20 +70,5 @@ function showCurrentQuestion() {
     // create for loops for questions and answers
 
 };
-
-let intervalId;
-
-// Timer function
-function startTimer () {
-    currentTime--;
-    timeLeft.textContent = currentTime;
-
-    if (currentTime === 0) {
-        clearInterval(intervalId);
-        alert("GAME OVER! Your final score is + result");
-    };
-};
-intervalId = setInterval(startTimer, 1000);
-
 
 // Input initials / Highscore page
